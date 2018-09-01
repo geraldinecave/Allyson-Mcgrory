5228103703a0a2617b997d9521108c0df5d533af
geraldinecave <geraldinemcave@gmail.com>
2018-09-01 10:56:31 +1000

    Gallery Scripts
    Added separate js file for gallery to remove waypoint error from other pages.

--- js/scripts.js
+++ js/scripts.js
@@ -8,132 +8,132 @@ $('#hamburger').click(function(){
 });
 
 
-// GALLERY
+// // GALLERY
 
-var twoCol;
+// var twoCol;
 
-var width = $(window).width();
+// var width = $(window).width();
 
-$(window).on('resize', function(){
-	if (typeof variable !== 'undefined'){
-	lightbox.destroy();
-	}
-   if($(this).width() != width){
-      width = $(this).width();
-      if (width < 992 && twoCol == false) {
-      	twoCol = true
-      	$('.gallery-col-1').html('')
-      	$('.gallery-col-2').html('')
-      	$('.gallery-col-3').html('')
-      	twoColLayout()
-      } else if (width > 992 && twoCol == true){
-      	twoCol = false
-      	$('.gallery-col-1').html('')
-      	$('.gallery-col-2').html('')
-      	$('.gallery-col-3').html('')
-      	threeColLayout()
-      }
-   }
-});
+// $(window).on('resize', function(){
+// 	if (typeof variable !== 'undefined'){
+// 	lightbox.destroy();
+// 	}
+//    if($(this).width() != width){
+//       width = $(this).width();
+//       if (width < 992 && twoCol == false) {
+//       	twoCol = true
+//       	$('.gallery-col-1').html('')
+//       	$('.gallery-col-2').html('')
+//       	$('.gallery-col-3').html('')
+//       	twoColLayout()
+//       } else if (width > 992 && twoCol == true){
+//       	twoCol = false
+//       	$('.gallery-col-1').html('')
+//       	$('.gallery-col-2').html('')
+//       	$('.gallery-col-3').html('')
+//       	threeColLayout()
+//       }
+//    }
+// });
 
-if (width < 992) {
-	twoColLayout()
-	twoCol = true
-} else {
-	threeColLayout()
-	twoCol = false
-}
+// if (width < 992) {
+// 	twoColLayout()
+// 	twoCol = true
+// } else {
+// 	threeColLayout()
+// 	twoCol = false
+// }
 
-function threeColLayout() {
+// function threeColLayout() {
 	
-	for( var i = 0; i <= 20; i++) {
+// 	for( var i = 0; i <= 20; i++) {
 
-		var imgBox = '<a id="image-' + i + '" class="image-box" href="images/big-img-' + i + '.jpg"><img src="images/img-' + i + '.jpg"/></a>';
+// 		var imgBox = '<a id="image-' + i + '" class="image-box" href="images/big-img-' + i + '.jpg"><img src="images/img-' + i + '.jpg"/></a>';
 
-		if (i < 7) {
+// 		if (i < 7) {
 
-			$('.gallery-col-1').append(imgBox);
+// 			$('.gallery-col-1').append(imgBox);
 			
-		} else if (i >= 7 && i < 14) {
+// 		} else if (i >= 7 && i < 14) {
 
-			$('.gallery-col-2').append(imgBox);
+// 			$('.gallery-col-2').append(imgBox);
 
-		} else {
-			$('.gallery-col-3').append(imgBox);
-		}
-	};
-
-
-	var waypoints = $('#image-18').waypoint({
-	  handler: function(direction) {
+// 		} else {
+// 			$('.gallery-col-3').append(imgBox);
+// 		}
+// 	};
 
 
-		for( var j = 21; j <= 41; j++) {
+// 	var waypoints = $('#image-18').waypoint({
+// 	  handler: function(direction) {
 
-			var imgBox = '<a id="image-' + j + '" class="image-box" href="images/big-img-' + j + '.jpg"><img src="images/img-' + j + '.jpg" /></a>';
 
-			if (j < 28) {
+// 		for( var j = 21; j <= 41; j++) {
 
-				$('.gallery-col-1').append(imgBox);
+// 			var imgBox = '<a id="image-' + j + '" class="image-box" href="images/big-img-' + j + '.jpg"><img src="images/img-' + j + '.jpg" /></a>';
+
+// 			if (j < 28) {
+
+// 				$('.gallery-col-1').append(imgBox);
 				
-			} else if (j >= 28 && j < 35) {
+// 			} else if (j >= 28 && j < 35) {
 
-				$('.gallery-col-2').append(imgBox);
+// 				$('.gallery-col-2').append(imgBox);
 
-			} else {
-				$('.gallery-col-3').append(imgBox);
-				this.destroy();
-			}
-		};
+// 			} else {
+// 				$('.gallery-col-3').append(imgBox);
+// 				this.destroy();
+// 			}
+// 		};
 
-		var lightbox = $('.gallery-section a').simpleLightbox();
-	  }
-	})
-	var lightbox = $('.gallery-section a').simpleLightbox();
-}
+// 		var lightbox = $('.gallery-section a').simpleLightbox();
+// 	  }
+// 	})
+// 	var lightbox = $('.gallery-section a').simpleLightbox();
+// }
 
 
 
-// If screen is less than 992px
-function twoColLayout() {
+// // If screen is less than 992px
+// function twoColLayout() {
 	
-	for( var i = 0; i <= 20; i++) {
+// 	for( var i = 0; i <= 20; i++) {
 
-		var imgBox = '<a id="image-' + i + '" class="image-box" href="images/big-img-' + i + '.jpg"><img src="images/img-' + i + '.jpg" /></a>';
+// 		var imgBox = '<a id="image-' + i + '" class="image-box" href="images/big-img-' + i + '.jpg"><img src="images/img-' + i + '.jpg" /></a>';
 
-		if (i < 11) {
+// 		if (i < 11) {
 
-			$('.gallery-col-1').append(imgBox);
+// 			$('.gallery-col-1').append(imgBox);
 
-		} else {
-			$('.gallery-col-2').append(imgBox);
-		}
-	};
+// 		} else {
+// 			$('.gallery-col-2').append(imgBox);
+// 		}
+// 	};
 
-	var waypoints = $('#image-18').waypoint({
-	  handler: function(direction) {
+// 	var waypoints = $('#image-18').waypoint({
+// 	  handler: function(direction) {
 
 
-		for( var j = 21; j <= 41; j++) {
+// 		for( var j = 21; j <= 41; j++) {
 
-			var imgBox = '<a id="image-' + j + '" class="image-box" href="images/big-img-' + j + '.jpg"><img src="images/img-' + j + '.jpg" /></a>';
+// 			var imgBox = '<a id="image-' + j + '" class="image-box" href="images/big-img-' + j + '.jpg"><img src="images/img-' + j + '.jpg" /></a>';
 
-			if (j < 31) {
+// 			if (j < 31) {
 
-				$('.gallery-col-1').append(imgBox);
+// 				$('.gallery-col-1').append(imgBox);
 				
 
-			} else {
+// 			} else {
 				
-				$('.gallery-col-2').append(imgBox);
-				this.destroy();
-			}
-		};	    
-		var lightbox = $('.gallery-section a').simpleLightbox();
-	  }
-	})
-	var lightbox = $('.gallery-section a').simpleLightbox();
-}
+// 				$('.gallery-col-2').append(imgBox);
+// 				this.destroy();
+// 			}
+// 		};	    
+// 		var lightbox = $('.gallery-section a').simpleLightbox();
+// 	  }
+// 	})
+// 	var lightbox = $('.gallery-section a').simpleLightbox();
+// }
 
 // LIGHTBOX
 
